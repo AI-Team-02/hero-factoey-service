079b983c55a957242bec90c19283baded7382936
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-10-31 14:24:55 +0900

    feat(config): create RedisConfig #22 #23 - String, Object hashes 로 생성

---
+++ src/main/java/ai/herofactoryservice/config/redis/RedisConfig.java
@@ -0,0 +1,27 @@
+package ai.herofactoryservice.config.redis;
+
+import ai.herofactoryservice.login.dto.RefreshToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // 직렬화 설정
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(RefreshToken.class));  // RefreshToken 클래스 직접 지정
+
+        return template;
+    }
+}
