c471ec0335f42cef485ee65e489d757761eadea4
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-10-31 14:35:20 +0900

    feat(KakaoLoginController): add 토큰 재발급 & 로그아웃 처리 #22 #23

--- src/main/java/ai/herofactoryservice/login/controller/KakaoLoginController.java
+++ src/main/java/ai/herofactoryservice/login/controller/KakaoLoginController.java
@@ -1,18 +1,18 @@
 package ai.herofactoryservice.login.controller;
 
-
-import ai.herofactoryservice.login.dto.KakaoUserInfoResponseDto;
 import ai.herofactoryservice.login.dto.LoginResponseDto;
 import ai.herofactoryservice.login.service.KakaoLoginService;
+import ai.herofactoryservice.login.service.TokenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 
 @Controller
 @RequiredArgsConstructor
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Slf4j
 public class KakaoLoginController {
     private final KakaoLoginService kakaoLoginService;
+    private final TokenService tokenService;
 
     @GetMapping("/login")
     public String kakaoLogin() {
@@ -34,5 +35,23 @@ public class KakaoLoginController {
         log.info("Kakao callback received with code");
         log.debug("code = {}", code);
         return kakaoLoginService.processKakaoLogin(code);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@RequestHeader("Authorization") String bearerToken,
+                                       @RequestHeader("Refresh-Token") String refreshToken) {
+        String accessToken = bearerToken.substring(7);
+        tokenService.logout(accessToken, refreshToken);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponseDto> refreshToken(
+            @RequestHeader("Authorization") String bearerToken,
+            @RequestHeader("Refresh-Token") String refreshToken
+    ) {
+        String accessToken = bearerToken.substring(7);
+        LoginResponseDto newTokens = tokenService.refreshTokens(accessToken, refreshToken);
+        return ResponseEntity.ok(newTokens);
     }
 }
