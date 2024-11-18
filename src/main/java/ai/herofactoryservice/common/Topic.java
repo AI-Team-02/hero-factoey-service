442de2cabd08d5e83ffbca80580e4105fd40cee7
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-11-18 16:07:59 +0900

    feat(kafka): create kafka config #50 - 컨슈머 재시도 로직 지수방식으로 3번 반복 - 프로듀서 멱등성, 메시지 안정성 보장

---
+++ src/main/java/ai/herofactoryservice/common/Topic.java
@@ -0,0 +1,7 @@
+package ai.herofactoryservice.common;
+
+public class Topic {
+    public static final String ORIGINAL_TOPIC= "item.original";
+    public static final String INSPECTED_TOPIC = "item.inspected";
+
+}
